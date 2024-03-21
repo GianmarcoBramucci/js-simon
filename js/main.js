d8757b6ad016b947ca66cabcb1bdc9e1544b6efc
GianmarcoBramucci <giangian797@gmail.com>
2024-03-21 16:02:34 +0100

    add feat

--- js/main.js
+++ js/main.js
@@ -6,16 +6,57 @@ let divRisposta = document.getElementById('risposta');
 let numGame = 5;
 let arraytCpu= [];
 let arrayUser= [];
+let score = 0;
+let count = 0;
+let time = 9000;
+
 gameInput.classList.add('d-none');
 
-
-btnStart.addEventListener('click',function(){
+btnStart.addEventListener('click',function(event1){
+    arraytCpu= [];
+    arrayUser= [];
+    score = 0;
+    count = 0;
     setupGame(arraytCpu,numGame,divRisposta);
-    setTimeout(removeElement,9000);
-    
+    setTimeout(removeElement,time);
     console.log(arraytCpu);
+    event1.stopPropagation();
 });
 
+btn,addEventListener('click',function(event2){
+    if(count<numGame){
+        let numUser = parseInt(input.value);
+        divRisposta.innerHTML= `fra devi rimette i numeri che hai visto uno alla volta`;
+        console.log(numUser);
+        if(!isNaN(numUser)){
+            count++;
+            if(arraytCpu.includes(numUser)){
+                score++
+            }
+
+            divRisposta.innerHTML= `fra hai inserito ${count} numeri e il tuo punteggio e: ${score}`; 
+
+            if(count == numGame){
+                gameInput.classList.add('d-none');
+                if(score == numGame){
+                    divRisposta.innerHTML= 'fra hai vinto';
+                }
+                else{
+                    divRisposta.innerHTML= `fra hai perso e il tuo punteggio e: ${score}`;
+                }
+            }     
+        }
+        else{
+            divRisposta.innerHTML= 'fra ma che hai scritto';
+        }
+    }
+    
+    input.value = '';
+    event2.stopPropagation();
+});
+input.addEventListener('click',function(event3){
+    event3.stopPropagation();
+});
 
 function setupGame(array,num,counteiner){
     let count = 0;
@@ -29,4 +70,5 @@ function setupGame(array,num,counteiner){
 
 function removeElement(){
 divRisposta.innerHTML = ''
+gameInput.classList.remove('d-none');
 }
